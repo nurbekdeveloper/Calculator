c68d2a155799a7723fb9f2fd98b2578064c784ec
nurbekdeveloper <nurbeknormatov02@gmail.com>
2023-03-01 02:50:51 -0800

    adding arifmetik operators

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter your numbers ");
+float fristnumber = Convert.ToInt32(Console.ReadLine());
+float secondnumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Result adding {fristnumber + secondnumber}");
+System.Console.WriteLine($"Result adding {fristnumber-secondnumber}");
+System.Console.WriteLine($"Result substring {fristnumber/secondnumber}");
+System.Console.WriteLine($"Result substring  {fristnumber*secondnumber}");
+System.Console.WriteLine($"Result substring  {fristnumber%secondnumber}");
+
+
+
+
