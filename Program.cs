a60d7501ea8ebd363390ec4dd422bcc712787056
nurbekdeveloper <nurbeknormatov02@gmail.com>
2023-03-02 02:23:28 -0800

    adding if else

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿System.Console.WriteLine("Enter your numbers ");
+﻿System.Console.WriteLine("Enter your firistnumber ");
 float fristnumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter your secondnumber ");
 float secondnumber = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"Result adding {fristnumber + secondnumber}");
-System.Console.WriteLine($"Result adding {fristnumber-secondnumber}");
-System.Console.WriteLine($"Result substring {fristnumber/secondnumber}");
-System.Console.WriteLine($"Result substring  {fristnumber*secondnumber}");
-System.Console.WriteLine($"Result substring  {fristnumber%secondnumber}");
+
+if(fristnumber>secondnumber){
+    System.Console.WriteLine("firistnumber is large than secondnumber");
+}
+else if(fristnumber==secondnumber){
+    System.Console.WriteLine("firistnumber equal to secondnumber ");
+}
+else{
+    System.Console.WriteLine("firistnumber is small than secondnumber ");
+}
+
+
 
 
 
